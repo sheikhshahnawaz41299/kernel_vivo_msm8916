09aeb993b5eceb2ea1bc7fc756b033695777869d
sheikhshahnawaz41299 <sheikhshahnawaz5555@gmail.com>
2022-05-29 14:12:37 +0530

    drivers:base: Import Vivo changes

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -292,6 +292,9 @@ static const char * const fw_path[] = {
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
+#ifdef CONFIG_MACH_VIVO
+	"/system/etc",
+#endif
 	"/lib/firmware",
 	"/firmware/image"
 };
